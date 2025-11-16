297b1127698b7dee192af1c7c3dc03f17e0ba69c
gbAguiiar <gabriielmaciel17@gmail.com>
2025-11-15 21:42:05 -0300

    finalização da página "sobre"

---
+++ pages/sobre/app.js
@@ -0,0 +1,23 @@
+const track = document.querySelector('.carousel-track');
+const images = document.querySelectorAll('.carousel-track img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function updateCarousel() {
+    const width = images[0].clientWidth;
+    track.style.transform = `translateX(-${index * width}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    index = (index + 1) % images.length; 
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    index = (index - 1 + images.length) % images.length; 
+    updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);

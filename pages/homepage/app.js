e527516543d7a1ac0681e8d10a5db4dbb5f955b0
gbAguiiar <gabriielmaciel17@gmail.com>
2025-11-18 19:52:29 -0300

    versão final 2.0

--- pages/homepage/app.js
+++ pages/homepage/app.js
@@ -1,24 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // 1. Seleciona todos os títulos de serviço na página
+   
     const titulosServico = document.querySelectorAll('.titulo-servico');
 
-    // 2. Itera sobre cada título encontrado
+  
     titulosServico.forEach(titulo => {
-        // 3. Adiciona um "ouvinte de evento" (event listener) para o clique
+      
         titulo.addEventListener('click', function() {
-            
-            // 4. Encontra a descrição detalhada, que é o elemento irmão do título
-            // O elemento pai do 'titulo-servico' é o 'li'
-            // O elemento filho do 'li' que queremos é o 'descricao-detalhada'
+        
             const itemServico = this.closest('.item-servico');
             const descricao = itemServico.querySelector('.descricao-detalhada');
 
-            // 5. Alterna a propriedade 'display'
+        
             if (descricao.style.display === 'block') {
-                // Se estiver visível, esconde
+          
                 descricao.style.display = 'none';
             } else {
-                // Se estiver escondido, mostra
+          
                 descricao.style.display = 'block';
             }
         });
@@ -30,6 +27,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("menu");
 
     hamburguer.addEventListener("click", () => {
-        menu.classList.toggle("ativo"); // abre/fecha
+        menu.classList.toggle("ativo"); 
     });
 });
